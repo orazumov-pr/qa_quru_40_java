ab40c6bfe939c8ad04f04e5d9559ab9d2a8bede0
Oleg Razumov <orazumoff@gmail.com>
2026-02-15 17:48:41 +0300

    Add task 0

---
+++ src/main/java/guru/qa/Main.java
@@ -0,0 +1,20 @@
+package guru.qa;
+
+import static java.lang.System.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        int a=60;
+        int b=15;
+
+        out.println(a+b);
+        out.println(a-b);
+        out.println(a*b);
+        out.println(a/b);
+
+
+    }
+}
